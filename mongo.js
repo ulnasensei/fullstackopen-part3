c845b616bceb088706a88677893a7528aac69ed6
Ulna <30731799+ulnasensei@users.noreply.github.com>
2023-05-22 01:18:00 +0300

    output fix2 - 3.12: Command-line database

--- mongo.js
+++ mongo.js
@@ -23,6 +23,7 @@ const Person = mongoose.model("Person", personSchema);
 
 if (process.argv.length === 3) {
   Person.find({}).then((result) => {
+    console.log("phonebook");
     result.forEach((person) => console.log(person.name, person.number));
     mongoose.connection.close();
     process.exit(0);
