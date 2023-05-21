a731498c276d5e48c6d216daa665c48e88a3fc2e
Ulna <30731799+ulnasensei@users.noreply.github.com>
2023-05-22 00:29:42 +0300

    3.9 phonebook backend step9

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const morgan = require("morgan");
+const cors = require("cors")
 const app = express();
 
 app.use(express.json());
+app.use(cors())
+
+app.use(express.static('build'))
+
 morgan.token('body', function (req, res) { return JSON.stringify(req.body) })
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
@@ -93,7 +98,7 @@ app.get("/api/info", (request, response) => {
   response.send(`<p>Phonebook has info for ${count} people.</p><p>${date}</p>`);
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
