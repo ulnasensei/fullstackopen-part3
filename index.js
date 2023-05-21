def428349ded67ef80bc6c2cffa0e2752a70a79f
Ulna <30731799+ulnasensei@users.noreply.github.com>
2023-05-21 23:17:56 +0300

    3.5: Phonebook backend step5

--- index.js
+++ index.js
@@ -26,43 +26,57 @@ let persons = [
   },
 ];
 
+function getRandomID(max = 10000) {
+  return Math.floor(Math.random() * max);
+}
+
 app.get("/api/persons", (request, response) => {
   response.json(persons);
 });
 
 app.get("/api/persons/:id", (request, response) => {
-    const id = Number(request.params.id)
-    const person = persons.find(person => person.id === id);
+  const id = Number(request.params.id);
+  const person = persons.find((person) => person.id === id);
 
-    if(person){
-        response.json(person);
-    }
-    else{
-        response.status(404).json({status_code: 404, message: "Not Found"})
-    }
-})
+  if (person) {
+    response.json(person);
+  } else {
+    response.status(404).json({ status_code: 404, message: "Not Found" });
+  }
+});
+
+app.post("/api/persons", (request, response) => {
+  const { name, number } = request.body;
+  const id = getRandomID();
+  const person = {
+    name: name,
+    number: number,
+    id: id,
+  };
+  persons.push(person);
+  response.status(201).json(person);
+});
 
 app.delete("/api/persons/:id", (request, response) => {
-    const id = Number(request.params.id)
-    const person = persons.find(person => person.id === id);
+  const id = Number(request.params.id);
+  const person = persons.find((person) => person.id === id);
 
-    if(person){
-        persons = persons.filter(person => person.id !== id)
-        response.status(204).end()
-    }
-    else{
-        response.status(404).json({status_code: 404, message: "Not Found"})
-    }
-})
+  if (person) {
+    persons = persons.filter((person) => person.id !== id);
+    response.status(204).end();
+  } else {
+    response.status(404).json({ status_code: 404, message: "Not Found" });
+  }
+});
 
 app.get("/api/info", (request, response) => {
-    const count = persons.length;
-    const date = new Date();
+  const count = persons.length;
+  const date = new Date();
 
-    console.log(count, date);
+  console.log(count, date);
 
-    response.send(`<p>Phonebook has info for ${count} people.</p><p>${date}</p>`);
-})
+  response.send(`<p>Phonebook has info for ${count} people.</p><p>${date}</p>`);
+});
 
 const PORT = 3001;
 app.listen(PORT, () => {
