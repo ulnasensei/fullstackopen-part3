e320b63d183271871606f0e8cc13522a1751f906
Ulna <30731799+ulnasensei@users.noreply.github.com>
2023-05-22 03:13:52 +0300

    3.19*: Phonebook database, step7

--- models/person.js
+++ models/person.js
@@ -14,7 +14,11 @@ mongoose
     console.log("error connecting to MongoDB:", error.message);
   });
 const personSchema = new mongoose.Schema({
-  name: String,
+  name: {
+    type: String,
+    minLength: 3,
+    required: true
+  },
   number: String,
 });
 
